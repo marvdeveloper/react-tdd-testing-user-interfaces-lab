657d2ae37d3e5d24ab3912e35a68990befd13315
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-02 11:48:45 +0300

    Implement portfolio home page component with headings, image, bio, and links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Marvin</h1>
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Marvin profile photo"
+      />
+      <h2>About Me</h2>
+      <p>
+        I'm a software developer passionate about building web apps using
+        React.
+      </p>
+      <a href="https://github.com/marvinkadoyo">GitHub</a>
+      <a href="https://linkedin.com/in/marvinkadoyo">LinkedIn</a>
+    </div>
+  );
 }
 
 export default App;
